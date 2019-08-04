e84cb34696e77d07b9f037de933bd024b3125f19
Oct-opus <octave1.olivetti@epitech.eu>
2019-08-04 23:20:44 +0200

    Added google functions

--- main.py
+++ main.py
@@ -432,19 +432,122 @@ class Google(Screen_element):
                 print("Not dound refresh_token")
                 self.refresh_token = None
 
+        # Request device and user codes
+        def request_oauth_code(self, now):
+            data = {"client_id": self.client_id, "scope": self.scope}
+            response = self.ntw.request("POST", const.GOOGLE_OAUTH_CODE_URL, data)
+            if response is None:
+                print("Google oauth request ko")
+            elif "error_code" in response or "error" in response:
+                print("Google oauth request ko: error ", response)
+            else:
+                print("Google oauth authorization ok: ", response)
+                self.device_code = response["device_code"]
+                user_code = response["user_code"]
+                verification_url = response["verification_url"].split("google.com", 1)[
+                    1
+                ]
+                # Might crash if now + expires_in int overflow
+                self.expires_at = now + response["expires_in"]
+                self.max_time_check = response["interval"]
+                self.authorization_data = {
+                    "client_id": self.client_id,
+                    "client_secret": self.client_secret,
+                    "code": self.device_code,
+                    "grant_type": "http://oauth.net/grant_type/device/1.0",
+                }
+                print(
+                    self.device_code,
+                    user_code,
+                    verification_url,
+                    self.expires_at,
+                    self.max_time_check,
+                )
+                self.sc.set_memory(
+                    name="google_text",
+                    elem_type="str",
+                    content=(0, 2, "Go to google.com"),
+                )
+                self.sc.set_memory(
+                    name="google_url", elem_type="str", content=(0, 3, verification_url)
+                )
+                self.sc.set_memory(
+                    name="google_code", elem_type="str", content=(0, 4, user_code)
+                )
+                self.displayed = ["google_text", "google_url", "google_code"]
+                return False
+
+        # Check user authorization
+        def request_oauth_authorization(self, now):
+            response = self.ntw.request(
+                "POST", const.GOOGLE_OAUTH_TOKEN_URL, self.authorization_data
+            )
+            if response is None:
+                print("Google oauth request ko")
+            elif "error_code" in response or "error" in response:
+                print("Google oauth request ko: error ", response)
+            else:
+                self.access_token = response["access_token"]
+                self.refresh_token = response["refresh_token"]
+                self.token_type = response["token_type"]
+                self.expires_at = now + response["expires_in"]
+                for elem in self.displayed:
+                    self.sc.set_memory(name=elem, delete=True)
+                print("Google oauth authorized")
+                print(
+                    self.access_token,
+                    self.refresh_token,
+                    self.token_type,
+                    self.expires_at,
+                )
+
+                # Save refresh token to file for later use
+                with open(const.GOOGLE_REFRESH_TOKEN_FILE, "w") as refresh_token_file:
+                    print("refresh_token saved to file")
+                    refresh_token_file.write(self.refresh_token)
+                return True
+            return False
+
+        # Refresh user authorization
+        def request_oauth_refresh(self, now):
+            data = {
+                "refresh_token": self.refresh_token,
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+                "grant_type": "refresh_token",
+            }
+            response = self.ntw.request("POST", const.GOOGLE_OAUTH_TOKEN_URL, data)
+            if response is None:
+                print("Google oauth request ko")
+            elif "error_code" in response or "error" in response:
+                self.refresh_token = None
+                self.expires_at = 0
+                print("Google oauth request ko: error ", response)
+            else:
+                self.access_token = response["access_token"]
+                # self.expires_in = response["expires_in"]
+                self.expires_at = now + response["expires_in"]
+                self.token_type = response["token_type"]
+                print("Google oauth refreshed")
+                print(self.access_token, self.token_type, self.expires_at)
+                return True
+            return False
+
         def check_connected(self, now):
             time_before_expired = self.expires_at - now
-            if self.refresh_token is None and time_before_expired > 0:
-                print("Requesting auth")
-                return self.request_oauth_authorization(now)
-            elif self.refresh_token is None and self.device_code is None:
-                print("Requesting code")
-                return self.request_oauth_code(now)
-            elif self.access_token is not None and time_before_expired - 60 <= 0:
+            if self.refresh_token is None:
+                if time_before_expired > 0:
+                    print("Requesting auth")
+                    return self.request_oauth_authorization(now)
+                else:  # elif self.device_code is None:
+                    print("Requesting code")
+                    return self.request_oauth_code(now)
+            elif time_before_expired - 60 <= 0:
                 print("Requesting refresh")
                 return self.request_oauth_refresh(now)
             elif self.access_token is not None:
                 return True
+            print("Error")
             return False
 
     class Drive:
@@ -453,14 +556,51 @@ class Google(Screen_element):
             self.file_url = const.GOOGLE_DRIVE_URL
             self.file_id = None
 
+        def create_file(self, access_token):
+            print("Creating drive file")
+            data = {
+                "title": "automated_unread_mail.json",
+                "mimeType": "application/json",
+                "labels": {"trashed": True},
+            }
+            headers = {
+                "Authorization": "Bearer " + access_token,
+                "Content-Type": "application/json; charset=UTF-8",
+            }
+            response = self.ntw.request(
+                "POST",
+                self.file_url + "?uploadType=resumable",
+                data=data,
+                headers=headers,
+            )
+            if "alternateLink" in response:
+                print("Success creating file")
+                self.file_id = response["id"]
+            else:
+                print("Error creating file")
+                self.file_id = None
+
+        def check_file(self, access_token):
+            headers = {"Authorization": "Bearer " + access_token}
+            response = self.ntw.request(
+                "GET", self.file_url + "?maxResults=1", headers=headers
+            )
+            if "items" in response:
+                if len(response["items"]) == 0:
+                    self.create_file(access_token)
+                else:
+                    print("No need to create drive file")
+                    self.file_id = response["items"][0]["id"]
+            else:
+                self.file_id = None
+
         def get_file(self, access_token):
             headers = {"Authorization": "Bearer " + access_token}
-            result = self.ntw.request(
+            return self.ntw.request(
                 "GET",
                 self.file_url + "/" + self.file_id + "?alt=media",
                 headers=headers,
             )
-            return result
 
     def __init__(self, ntw, sc):
         Screen_element.__init__(self, ntw, sc, 0)
@@ -528,7 +668,7 @@ def main():
             google.get(now)
             # _thread.start_new_thread(google.get, (now,))
             # else:
-            # 	print("No internet connection")
+            #     print("No internet connection")
         sc.update_display()
         utime.sleep(const.MAIN_CYCLE_TIME)
 
--- readme.md
+++ readme.md
@@ -10,10 +10,10 @@ Next, Il be adding access to Gmail using OAuth2ForDevices from Google and a scri
 * Install MicroPython on your board.
 * In the consts_exemple.py file, replace the following values with yours
 ```
-NTW_LIST, WEATHER_API_KEY, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET.
+NTW_LIST, WEATHER_API_KEY, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 ```
 * Rename the consts_exemple.py into consts.py.
-* Upload the consts.py and the python files in the Libs folder at the root directory of your ESP32. You can use Ampy program to do so.
+* Upload the consts.py and the python files in the Libs folder at the root directory of your ESP32. You can use Ampy program to do so. You can edit the .ampy file to change the default config.
 * Run the main.py, you can use Ampy. For testing is like to use Esplorer.
 
 ## Working boards
@@ -31,7 +31,7 @@ The oled is not connected to the standard I2C pins, its SDA pin is 4 and SCL pin
 You can set this up by adding Wire.begin(4, 15) to your setup code. The display's I2C address is 0x3C. You will also need to use the OLED_RST pin to enable the display: pinMode(16,OUTPUT); digitalWrite(16, LOW); delay(50); digitalWrite(16, HIGH);
 After applying these settings I2CScan can find the display and most SSD1306 Arduino libraries work (for example the Adafruit SSD1306 works after changing Wire.begin() to Wire.begin(4, 15) and setting the reset pin and the I2C address).
 
-Dimensions
+Dimensions:
 25.5mm height
 50mm width
 5.75mm thickness
